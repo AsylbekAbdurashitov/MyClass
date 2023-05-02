7d127812914c231461e726598522e56577b83898
асылбек абдурашитов <asylbekabdurasitov@MacBook-Pro-asylbek.local>
2023-05-02 16:40:11 +0600

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,20 @@
+public class MyClass {
+
+    String name;
+    String surname;
+    int age;
+    String[] PeaksoftLesson = new String[4];
+    String tamak;
+
+    public MyClass(String name, String surname, int age, String[] Peaksoftlesson, String tamak) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.PeaksoftLesson = Peaksoftlesson;
+        this.tamak = tamak;
+    }
+
+    public MyClass(){
+
+    }
+}
